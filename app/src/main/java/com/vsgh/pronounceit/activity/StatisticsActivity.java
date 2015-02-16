8defe1bd1cf889b91e4d410843d971bd1ae654e8
VladVysotsky <wolltone@gmail.com>
2015-02-16 20:28:01 +0300

    Test2

--- app/src/main/java/com/vsgh/pronounceit/activity/StatisticsActivity.java
+++ app/src/main/java/com/vsgh/pronounceit/activity/StatisticsActivity.java
@@ -19,4 +19,9 @@ public class StatisticsActivity extends BaseVsghActivity {
     protected void configureViews() {
 
     }
+
+    protected void petya() {
+
+    }
+
 }
---
+++ app/src/main/java/com/vsgh/pronounceit/core/Downloader.java
@@ -0,0 +1,7 @@
+package com.vsgh.pronounceit.core;
+
+/**
+ * Created by Slawa on 2/16/2015.
+ */
+public class Downloader {
+}
