06b7769eb1b30c3f9fb3d470b7320cd556868172
Eren <narutrix@mail.ru>
2015-04-05 21:34:37 +0300

    Fixed bug change user with social network

--- app/src/main/java/com/vsgh/pronounceit/activity/GameActivity.java
+++ app/src/main/java/com/vsgh/pronounceit/activity/GameActivity.java
@@ -105,7 +105,7 @@ public class GameActivity extends BaseVsghActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, text);
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
